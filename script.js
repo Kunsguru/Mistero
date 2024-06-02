8d920daa302b0de755e1ee66021c9fb650821823
Kunsguru <kingsleym2024@gmail.com>
2024-06-02 15:26:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+setInterval(function() {
+        
+        var homeScore = Math.floor(Math.random() * 5);
+        
+        var drawScore = Math.floor(Math.random() * 5);
+        
+        var awayScore = Math.floor(Math.random() * 5);
+        
+        
+        document.getElementById('homeResult').innerText = homeScore;
+        
+        document.getElementById('drawResult').innerText = drawScore;
+        
+        document.getElementById('awayResult').innerText = awayScore;
+    }, 20000);
